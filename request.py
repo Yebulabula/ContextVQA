403693744eae92a1a7f856066b91742c0e0ad88e
Yebulabula <572615391@qq.com>
2024-07-24 18:04:59 +0100

    update

--- request.py
+++ request.py
@@ -3,5 +3,5 @@
 import os
 
 # move the first 10 folders from the root to the ContextVQA folder
-for folder in os.listdir('../Backup/3D_scans')[:10]:
+for folder in os.listdir('../Backup/3D_scans')[:50]:
     os.system(f'mv ../Backup/3D_scans/{folder} 3D_scans/{folder}')
