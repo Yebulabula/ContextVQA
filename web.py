7f040f08c50985ea98d93039088be7d8bbec83b1
Yebulabula <572615391@qq.com>
2024-07-26 00:58:26 +0100

    webb

--- web.py
+++ web.py
@@ -3,67 +3,35 @@ import plotly.graph_objects as go
 import os
 import numpy as np
 import json
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload, MediaInMemoryUpload
-import io
+import firebase_admin
+from firebase_admin import credentials, firestore
 import base64
 
-# Initialize Google Drive authentication
-def authenticate_google_drive():
-    try:
-        scope = ['https://www.googleapis.com/auth/drive']
-        # Load and decode the service account credentials from environment variable
-        service_account_info = os.getenv('GOOGLE_SERVICE_ACCOUNT')
-        if not service_account_info:
-            raise ValueError("Google service account credentials not found in environment variables.")
-        
-        # Decode the base64 encoded JSON service account key
-        decoded_credentials = base64.b64decode(service_account_info)
-        print(decoded_credentials)
-        credentials = service_account.Credentials.from_service_account_info(
-            json.loads(decoded_credentials), scopes=scope
-        )
-        service = build('drive', 'v3', credentials=credentials)
-        st.success("Google Drive authentication successful!")
-        return service
-    except Exception as e:
-        st.error(f"Failed to authenticate Google Drive: {e}")
-        return None
+# Initialize Firebase
+firebase_credentials = os.getenv('FIREBASE_CREDENTIALS')
 
-# Search for the file in Google Drive
-def search_file(service, file_name, folder_id=None):
-    try:
-        query = f"name='{file_name}' and trashed=false"
-        if folder_id:
-            query += f" and '{folder_id}' in parents"
-        results = service.files().list(q=query, fields="files(id, name)").execute()
-        items = results.get('files', [])
-        if items:
-            return items[0]  # Return the first matching file
-        else:
-            return None
-    except Exception as e:
-        st.error(f"An error occurred during file search: {e}")
-        return None
+if firebase_credentials:
+    # Decode the base64 encoded credentials
+    cred_json = base64.b64decode(firebase_credentials).decode('utf-8')
+    cred_dict = json.loads(cred_json)
+    cred = credentials.Certificate(cred_dict)
 
-# Upload or update file in Google Drive
-def upload_or_update_file(service, file_name, content, folder_id=None):
-    try:
-        file = search_file(service, file_name, folder_id)
-        media = MediaInMemoryUpload(content.encode(), mimetype='application/json')
-        if file:
-            # Update the existing file
-            file_id = file['id']
-            updated_file = service.files().update(fileId=file_id, media_body=media).execute()
-        else:
-            # Create a new file
-            file_metadata = {'name': file_name}
-            if folder_id:
-                file_metadata['parents'] = [folder_id]
-            new_file = service.files().create(body=file_metadata, media_body=media, fields='id, name, parents, webViewLink').execute()
-    except Exception as e:
-        st.error(f"An error occurred during file upload/update: {e}")
+    # Initialize Firebase app if not already initialized
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+
+# Connect to Firestore
+db = firestore.client()
+
+# Function to save context data to Firestore
+def save_context_data(data):
+    db.collection('ContextReason').add(data)
+    
+# Function to get context data from Firestore
+def get_context_data():
+    docs = db.collection('context_data').stream()
+    data = [doc.to_dict() for doc in docs]
+    return data
 
 # Streamlit app configuration
 st.set_page_config(
@@ -115,36 +83,8 @@ def initialize_plot(vertices, triangles, vertex_colors, instance_labels, id2labe
 
     return fig
 
-
-def load_context_data(service, file_id):
-    try:
-        request = service.files().get_media(fileId=file_id)
-        file_data = io.BytesIO()
-        downloader = MediaIoBaseDownload(file_data, request)
-
-        done = False
-        while not done:
-            status, done = downloader.next_chunk()
-
-        file_data.seek(0)
-        return json.load(file_data)
-    except Exception as e:
-        st.error(f"An error occurred while loading context data: {e}")
-        return None
-
-
-def save_context_data(data, service=None, folder_id=None):
-    json_data = json.dumps(data, indent=4)
-    if service:
-        upload_or_update_file(service, 'context_data.json', json_data, folder_id)
-    else:
-        with open('context_data.json', 'w') as json_file:
-            json_file.write(json_data)
-        st.success("Data saved locally.")
-
 # Initialize state only once
-def initialize_state(service):
-    #先初始化
+def initialize_state():
     if 'clicked' not in st.session_state:
         st.session_state.clicked = False
 
@@ -152,9 +92,8 @@ def initialize_state(service):
         st.session_state.selected_label = None
 
     if 'data' not in st.session_state:
-        file = search_file(service, 'context_data.json')
-        data = load_context_data(service, file['id']) if file else []
-        st.session_state.data = data
+        data = get_context_data()
+        st.session_state.data = data if data else []
 
     if 'responses_submitted' not in st.session_state:
         st.session_state.responses_submitted = 0
@@ -162,8 +101,7 @@ def initialize_state(service):
     if 'question_id' not in st.session_state:
         st.session_state.question_id = int(st.session_state.data[-1]['question_id']) + 1 if st.session_state.data else 0
 
-drive_service = authenticate_google_drive()
-initialize_state(drive_service)
+initialize_state()
 
 # Selectbox for scene selection
 scene_id = st.selectbox("Select a Scene ID", list(SCENE_ID_TO_FILE.keys()))
@@ -256,12 +194,10 @@ if scene_id:
                 'answer': answer,
             }
             st.session_state.question_id += 1
-            st.session_state.data.append(entry)
-
+            st.session_state.responses_submitted += 1
             st.success("Submitted successfully!")
             st.balloons()
-            drive_service = authenticate_google_drive()
-            save_context_data(st.session_state.data, service=drive_service, folder_id='1IBtc__spB7umj7DLlUJFecEXGy8lQ6Uy')
+            save_context_data(entry)
 
     final_section_html = f"""
     <div style='margin-top: 20px;'>
